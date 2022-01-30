eed4b53f1f0d464c68d67f82ab551a8e74b5c237
Zhu Bicen <zhubicen@gmail.com>
2022-01-30 15:10:47 +0800

    Initial Commit

---
+++ animationTest/animationTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  animationTestApp.swift
+//  animationTest
+//
+//  Created by Bicen Zhu on 2022/1/30.
+//
+
+import SwiftUI
+
+@main
+struct animationTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
