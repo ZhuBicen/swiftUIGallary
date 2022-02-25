2b723bdb85a31bb429e5e3a3db59fce86dd0c4fe
Zhu Bicen <zhubicen@gmail.com>
2022-02-25 21:46:39 +0800

    Try outline around positioned text

---
+++ animationTest/SwiftUIView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  animationTest
+//
+//  Created by Bicen Zhu on 2022/2/25.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
